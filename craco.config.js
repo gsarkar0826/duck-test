ed5858df333b573339613e60796dd27b13d19cc7
Gautam Sarkar <gsarkar@splunk.com>
2022-08-08 15:17:00 -0700

    update craco config

--- craco.config.js
+++ craco.config.js
@@ -1,42 +1,28 @@
-const path = require('path');
-const wasmExtensionRegExp = /\.wasm$/;
+const { addBeforeLoader, loaderByName } = require('@craco/craco');
 
 module.exports = {
   webpack: {
-    configure: (webpackConfig, { env, paths }) => {
-      // Emscripten uses import.meta
-      webpackConfig.module.rules.push({
-        test: /\.js$/,
-        loader: require.resolve('@open-wc/webpack-import-meta-loader'),
-      });
+    configure: (webpackConfig) => {
+      const wasmExtensionRegExp = /\.wasm$/;
+      webpackConfig.resolve.extensions.push('.wasm');
 
-      webpackConfig.module.rules.forEach(rule => {
-        (rule.oneOf || []).forEach(oneOf => {
+      webpackConfig.module.rules.forEach((rule) => {
+        (rule.oneOf || []).forEach((oneOf) => {
           if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
-            // make default file-loader ignore WASM files
             oneOf.exclude.push(wasmExtensionRegExp);
           }
         });
       });
 
-      // We want to provide "require('somefile.wasm')" as Emscripten locateFile.
-      // This makes sure files required this way are not altered.
-      webpackConfig.module.rules.push({
+      const wasmLoader = {
         test: /\.wasm$/,
-        type: 'javascript/auto',
-        include: path.resolve(__dirname, 'src'),
-        loaders: 'file-loader',
-        options: {
-          name: '[name].[hash:8].[ext]',
-        },
-      });
+        exclude: /node_modules/,
+        loaders: ['wasm-loader'],
+      };
+
+      addBeforeLoader(webpackConfig, loaderByName('file-loader'), wasmLoader);
 
       return webpackConfig;
     },
   },
-  eslint: {
-    configure: {
-      "ignorePatterns": ["src/wasm/*"],
-    },
-  },
-}
+};
