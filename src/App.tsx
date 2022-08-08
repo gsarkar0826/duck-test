6d3e4c444fb193f14486cab95e589c00b251c9ae
Gautam Sarkar <gsarkar@splunk.com>
2022-08-07 23:36:37 -0700

    add duckdb wasm and craco

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import { FC, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as duckdb from "@duckdb/duckdb-wasm";
+import duckdb_wasm from "@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm";
+import duckdb_wasm_eh from "@duckdb/duckdb-wasm/dist/duckdb-eh.wasm";
 
-function App() {
+// select a bundle based on browser checks
+const DUCKDB_BUNDLES: duckdb.DuckDBBundles = {
+  mvp: {
+    mainModule: duckdb_wasm,
+    mainWorker: new URL(
+      "@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js",
+      import.meta.url
+    ).toString(),
+  },
+  eh: {
+    mainModule: duckdb_wasm_eh,
+    mainWorker: new URL(
+      "@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js",
+      import.meta.url
+    ).toString(),
+  },
+};
+
+const App: FC = ({}) => {
+  useEffect(() => {
+    const resolveDatabase = async () => {
+      const bundle = await duckdb.selectBundle(DUCKDB_BUNDLES);
+      // init async duckdb-wasm
+      const worker = new Worker(bundle.mainWorker!);
+      const logger = new duckdb.ConsoleLogger();
+      const db = new duckdb.AsyncDuckDB(logger, worker);
+      await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+
+      // establish connection
+      const conn = await db.connect();
+      // todo: connect with .db file
+      const table = await conn.query(`SELECT 42;`);
+      console.log(table);
+    };
+
+    resolveDatabase();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +62,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
